0da1e988f0ee94b0b66e80bc17b59decd811b648
Jake Burzlaff <jake.bilbao.04.06@gmail.com>
2020-08-04 21:58:37 -0600

    reinitialize passLength variable and update readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # password-generator
+
+ - I used a function from Mozilla Developer Network to generate a random integer between two variables that is inclusive.
+ [Mozilla Page](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random)
--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -13,8 +13,7 @@ function writePassword() {
 
 //Function to generate the password
 function generatePassword() {
-  var passLength = 0;
-
+var passLength = 0;
   //Error check for upper and lower bound on password length
   do {
     passLength = prompt("Please choose a password length between 8 and 128 characters")
@@ -23,42 +22,81 @@ function generatePassword() {
 
   //Define character types object and set bool to false and strings to define characters
   var charTypes = {
-    lower: false,
+    lowerUsed: false,
     lowerStr: 'abcdefghijklmnopqrstuvwxyz',
-    upper: false,
+    lowerCharCtr: 0,
+    upperUsed: false,
     upperStr: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
-    nums: false,
+    upperCharCtr: 0,
+    numsUsed: false,
     numsStr: '1234567890',
-    special: false,
+    numsCharCtr: 0,
+    specialUsed: false,
     specialStr: ' !"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~',
+    specialCharCtr: 0,
   }
 
 
   //Error check to make sure user selects at least one type of character
   do {
-    charTypes.lower = confirm("Would you like lowercase letters used?");
-    charTypes.upper = confirm("Would you like uppercase letters used?");
-    charTypes.nums = confirm("Would you like numbers used?");
-    charTypes.special = confirm("Would you like special characters used?");
+    charTypes.lowerUsed = confirm("Would you like lowercase letters used?");
+    charTypes.upperUsed = confirm("Would you like uppercase letters used?");
+    charTypes.numsUsed = confirm("Would you like numbers used?");
+    charTypes.specialUsed = confirm("Would you like special characters used?");
 
-    if (charTypes.lower === false && charTypes.upper === false && charTypes.nums === false && charTypes.special === false) {
+    if (charTypes.lowerUsed === false && charTypes.upperUsed === false && charTypes.numsUsed === false && charTypes.specialUsed === false) {
       alert("You must select at least one type of character for your password");
     }
-  } while (charTypes.lower === false && charTypes.upper === false && charTypes.nums === false && charTypes.special === false);
+  } while (charTypes.lowerUsed === false && charTypes.upperUsed === false && charTypes.numsUsed === false && charTypes.specialUsed === false);
 
   //Logic to generate password
 
-  //set character type arrays
+  //Logic to determine how many of each type of character will be used
+  //Set each character type to have at least one character in string if character type is used and increment total counter
+  var currentCharCtr = 0;
+  var charsLeft = passLength;
+  if (charTypes.lowerUsed === true) {
+    charTypes.lowerCharCtr = 1;
+    currentCharCtr++;
+  }
+  if (charTypes.upperUsed === true) {
+    charTypes.upperCharCtr = 1;
+    currentCharCtr++;
+  }
+  if (charTypes.numsUsed === true) {
+    charTypes.numsCharCtr = 1;
+    currentCharCtr++;
+  }
+  if (charTypes.specialUsed === true) {
+    charTypes.specialCharCtr = 1;
+    currentCharCtr++;
+  }
 
-  var lowerArr = ['a']
-  console.log(charTypes.lower);
-  console.log(charTypes.upper);
-  console.log(charTypes.nums);
-  console.log(charTypes.special);
-  console.log(charTypes.specialStr.length);
-  console.log(charTypes.numsStr.length);
-  console.log(charTypes.upperStr.length);
-  console.log(charTypes.lowerStr.length);
+  charsLeft = charsLeft - currentCharCtr;
+  
+  //Get a random integer between two values that includes those values
+  function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+  }
+
+  //Generate a random number of lowercase characters
+  getRandomIntInclusive(charTypes.lowerCharCtr,charsLeft);
+
+  //Generate random character from each character type
+  var lowerRand = Math.floor(Math.random() * charTypes.lowerStr.length);
+  var upperRand = Math.floor(Math.random() * charTypes.upperStr.length);
+  var numsRand = Math.floor(Math.random() * charTypes.numsStr.length);
+  var specialRand = Math.floor(Math.random() * charTypes.specialStr.length);
+
+  console.log(charTypes.lowerStr[lowerRand]);
+  console.log(charTypes.upperStr[upperRand]);
+  console.log(charTypes.numsStr[numsRand]);
+  console.log(charTypes.specialStr[specialRand]);
+  console.log(totalCharCtr);
+  
+
 
   return "Generating your password....";
 }
